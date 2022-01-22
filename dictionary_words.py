fcd36988abb85777800251e12f6552b66da14ac1
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-01-21 16:17:19 -1000

    improved dictionary

--- dictionary_words.py
+++ dictionary_words.py
@@ -2,11 +2,15 @@ import random
 import sys, random
 
 def random_dict_words():
+
+    #default file is the dictionary if not declared 
+    file = "/usr/share/dict/words"
+
     #take in the argument for number of words to pull from the dictionary
     num_words = int(sys.argv[1])
-
+    
     #open the dictionary file and separate into a list at each line break
-    dict = open("/usr/share/dict/words", "r")
+    dict = open(file, "r")
     lines = dict.read().splitlines()
 
     #initialize an empty array for our random words
@@ -25,6 +29,7 @@ def random_dict_words():
 
     dict.close()
 
-#call the function
-random_dict_words()
+# name is the function being imported // main is the function called on the command line
+if __name__ == "__main__":
+    random_dict_words()
 
