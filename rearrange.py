efe01fab9ee06f6c5d5f1128cb73f5bf9e7e0bc7
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-01-21 14:56:27 -1000

    updated rearrange

--- rearrange.py
+++ rearrange.py
@@ -1,16 +1,13 @@
 import random
 
-from sqlalchemy import null
-
-
-words = input('write words to rearrange:').split()
+#from sqlalchemy import null
 
 def rearrange_words():
+    words = input('write words to rearrange:').split()
     random.shuffle(words)
     new_sequence = " ".join(words)
     print(new_sequence)
 
 if __name__ == '__main__':
-    quote = rearrange_words()
-    print (quote)
+    rearrange_words()
 
