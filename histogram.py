757cbd3ef7a17a90eb3e23ab0f5a962cb6bd9b12
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-01-26 14:57:46 -1000

    fixed bug to check frequency of word not in dictionary

--- histogram.py
+++ histogram.py
@@ -2,14 +2,9 @@ import random
 import sys, random
 from string import punctuation
 
-class histo_word:
-    def __init__ (self, word, count):
-        self.word = word
-        self.count = count
-
 
 def histogram(file):
-
+ 
     #take in a text file as an argument
     #file = sys.argv[1]
     
@@ -18,7 +13,6 @@ def histogram(file):
 
     #separate into words at each space
     words = file.read().split(" ")
-    
 
     #initialize an empty dictionary
     #key = word, value = appearance count
@@ -27,16 +21,17 @@ def histogram(file):
     #remove any punctuation from beginning/end of words 
     for word in words:
         #strip the punctionation from both sides of the word & convert to lowercase
-        word = word.lstrip(punctuation).rstrip(punctuation).lower()
+        word = word.lstrip(punctuation).rstrip(punctuation).strip('\n').lower()
+        #add or increment in the dictionary
         if word not in dictionary:
-            dictionary[word]=1 
+            dictionary[word] = 1 
         else:
             dictionary[word] += 1
 
 
     for word in dictionary:
         print(f'{word} = {dictionary[word]}') 
-
+    
     unique_words(dictionary)
 
     frequency('bad', dictionary)
@@ -49,9 +44,12 @@ def unique_words(dictionary):
     
 
 def frequency(word, dictionary):
-    
-    #find the key value pair in the dictionary
-    print(f'\nThe word {word} appears {dictionary[word]} times in this text.\n')
+
+    #add a default word frequency of zero
+    if word in dictionary:
+        print(f'\nThe word {word} appears {dictionary[word]} times in this text.\n')
+    else:
+        print(f'\nThe word {word} appears 0 times in this text.\n')
 
 
 
